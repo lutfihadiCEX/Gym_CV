3986f3beaa27ba176cdffbc1932917a47f194f59
lutfihadiCEX <lutfihadij@gmail.com>
2025-12-16 21:04:26 +0800

    Added YOLOv8 model path)

--- src/detector.py
+++ src/detector.py
@@ -1,30 +1,38 @@
+from ultralytics import YOLO
 import cv2
-import mediapipe as mp
 
 class PoseDetector:
-    def __init__(self):
-        self.mp_pose = mp.solutions.pose
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.pose = self.mp_pose.Pose(
-            static_image_mode=False,
-            model_complexity=1,                  # 1 = better accuracy (use 2 if too slow)
-            smooth_landmarks=True,
-            min_detection_confidence=0.4,        # Higher = stricter detection (helps avoid leg confusion)
-            min_tracking_confidence=0.4
-        )
+    def __init__(self, model_path='yolov8n-pose.pt'):
+
+        self.model = YOLO(model_path)
+        self.right_arm_indices = [6, 8, 10] # Shoulder, Elbow, Wrist
+        self.right_arm_ids = {6: 'RShoulder', 8: 'RElbow', 10: 'RWrist'}
 
     def process(self, image):
-        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        rgb.flags.writeable = False
-        self.results = self.pose.process(rgb)
-        rgb.flags.writeable = True
-        return self.results
+        # Run inference, conf=0.5 
+        results = self.model(image, stream=False, verbose=False, conf=0.5)
+        self.results = results
+        
+        # Keypoints for 1st detected human
+        if results and len(results[0].keypoints.data) > 0:
+            # Use data tensor -> keypoints
+            import numpy as np
+            keypoints_tensor = results[0].keypoints.data[0]             #  (17, 3) tensor
+            
+            xy = results[0].keypoints.xyn[0].cpu().numpy()              #  Normalized x, y (shape 17, 2)
+            
+            conf = results[0].keypoints.conf[0].cpu().numpy()           #  Confidence scores (shape 17)
+            
+            
+            keypoints_xyc = np.hstack((xy, conf[:, None]))              #  (17, 3) array: [x, y, conf]
+            
+            return keypoints_xyc
+            
+        return None
 
     def draw(self, image):
-        if self.results.pose_landmarks:
-            self.mp_drawing.draw_landmarks(
-                image, self.results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS,
-                landmark_drawing_spec=self.mp_drawing.DrawingSpec(color=(0, 255, 255), thickness=4, circle_radius=4),  # Yellow dots
-                connection_drawing_spec=self.mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=3)
-            )
+        # Bounding boxes on human
+        if self.results:
+            annotated_frame = self.results[0].plot()
+            return annotated_frame
         return image
